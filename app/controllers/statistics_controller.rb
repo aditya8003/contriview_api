8cdc42f84395218f944500d34c365eeca01553e1
Stefan Hartmann <stefanhartmann@gmail.com>
2017-07-14 22:58:26 -0400

    make sure to parse get request

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,7 +1,8 @@
 class StatisticsController < ApplicationController
 
   def index
-    render json: "hello"
+    @stats = HTTParty.get("http://api.census.gov/data/2015/acs5?get=B05001_002E,B05001_005E,B05001_006E&for=county:*&key=#{ENV["CENSUS_KEY"]}")
+    render json: @stats.body  
   end
 
   def show
