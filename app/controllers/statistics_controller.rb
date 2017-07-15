1c55b188d4c9d8da3f2488fe7c1d1dcfdd1988d1
Stefan Hartmann <stefanhartmann@gmail.com>
2017-07-15 15:11:39 -0400

    build much better index method

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,10 +1,18 @@
 class StatisticsController < ApplicationController
 
   def index
+    states = AmericanState.includes(:populations).all
+    @states = states.map { |state| 
+      {
+        state: state.state_name,
+        total_population: state.populations.map { |p| p.total_pop }.sum, 
+        immigrant_population: state.populations.map {|p| p.naturalized_citizens + p.non_citizens }.sum 
+      }
+    }
+    render json: @states
   end
 
   def show
-    state = AmericanState.include(:populations).where(state_name: params[:state]).first
   end
 
 end
