86c52779b4ba145ed685c719a5287b989dd1bfca
Stefan Hartmann <stefanhartmann@gmail.com>
2017-07-14 23:18:00 -0400

    add redis

--- Gemfile
+++ Gemfile
@@ -7,7 +7,9 @@ end
 
 gem 'rack-cors', :require => 'rack/cors'
 gem 'httparty'
+gem 'redis', '~> 3.0'
 gem 'redis-rails'
+gem 'redis-namespace'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.4'
 # Use postgresql as the database for Active Record
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,8 +1,16 @@
 class StatisticsController < ApplicationController
 
   def index
-    @stats = HTTParty.get("http://api.census.gov/data/2015/acs5?get=B05001_002E,B05001_005E,B05001_006E&for=county:*&key=#{ENV["CENSUS_KEY"]}")
-    render json: @stats.body  
+    stats = $redis.get("stats")
+    if stats.nil?
+      stats = HTTParty.get("http://api.census.gov/data/2015/acs5?get=B05001_002E,B05001_005E,B05001_006E&for=county:*&key=#{ENV["CENSUS_KEY"]}")
+      $redis.set("stats", stats.body)
+      $redis.expire("stats", 300.seconds.to_i)
+      @stats = stats.body
+    else
+      @stats = stats
+    end
+    render json: @stats
   end
 
   def show
